f103e1c38ce7a02fb59618bd1465154d7c78e366
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-07-11 19:50:06 +0200

    Bot works properly and without errors. Up to date with todo list.

--- bot.py
+++ bot.py
@@ -1,42 +1,88 @@
 import re
-from bs4 import BeautifulSoup
 import requests
-# from IPython import embed
 import telepot
 from time import sleep
+from bs4 import BeautifulSoup
+from yaml import load
+# from IPython import embed
 
-token = '443609818:AAG65ovh6ywr-XyuXy53KHS5ZP1oPrTyf8o'
+with open('config.yaml') as f:
+    config = load(f)
+
+token = config['token']
 bot = telepot.Bot(token)
-currencies = [
-        'bitcoin',
-        'ethereum',
-        ]
+
+currencies = [  # supported cryptocurrencies
+    'bitcoin',
+    'ethereum',
+    ]
+commands = [  # implemented telegram commands
+    '/start',
+    '/bitcoin',
+    '/ethereum',
+    ]
+
+# fallback answer if messges are not understood
+guide = 'Use either '
+for command in commands:
+    if command != commands[-1]:
+        guide += command + ' or '
+    else:
+        guide += command + '.'
 
 
-def price(currency):
+def get_price(currency):
     '''Returns up-to-date price of currency.'''
-    url = 'http://www.finanzen.net/devisen/{currency}-euro-kurs'.format(currency=currency)
+
+    url = 'http://www.finanzen.net/devisen/{currency}-euro-kurs'.format(
+        currency=currency)
     response = requests.get(url)
+
     soup = BeautifulSoup(response.text, 'html.parser')
-    div = soup.find("div", {"class": "col-xs-5 col-sm-4 text-sm-right text-nowrap"})
+    div = soup.find(
+        "div", {"class": "col-xs-5 col-sm-4 text-sm-right text-nowrap"})
+
     x = r'[0-9]*\.*[0-9]{1,3}\,[0-9]{1,4}'
     price = re.findall(pattern=x, string=str(div))[0]
+
     answer = '{curr} price is {price} EUR'.format(curr=currency, price=price)
     return answer
 
 
+def fallback(chat_id):
+    '''Send fallback message if something is not quite right.'''
+    bot.sendMessage(chat_id=chat_id, text=guide)
+
+
 def handle(msg):
+
     content_type, chat_type, chat_id = telepot.glance(msg)
-    if msg['text'] == '/bitcoin' or msg['text'] == '/ethereum':
-        bot.sendMessage(chat_id=chat_id, text=price(msg['text'][1:]))
-    # elif msg['text'] == '/start':
+
+    if content_type == 'text':
+        message = msg['text']
     else:
-        bot.sendMessage(chat_id=chat_id, text='Use either /bitcoin or /ethereum')
+        fallback(chat_id)
+        return
+
+    if message.startswith('/'):
+        command = message[1:]
+    else:
+        fallback(chat_id)
+        return
+
+    if command in currencies:
+        bot.sendMessage(chat_id=chat_id, text=get_price(command))
+    else:
+        fallback(chat_id)
+        return
+
+    return
 
 
 # Keep the bot listening and running
 print('listening ...')
 bot.message_loop(handle)
+
 while True:
     sleep(10)
     # for currency in currencies:
