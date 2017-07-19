181107a9ce9b2e64379c3f9fa40c7ba41a3975b9
Noah Biederbeck <noah.biederbeck@udo.edu>
2017-07-19 20:24:41 +0200

    Get it working

--- bot.py
+++ bot.py
@@ -87,10 +87,10 @@ def handle(msg):
 # Keep the bot listening and running
 print('listening ...')
 bot.message_loop(handle)
-sleep(24 * 60 * 60 - 1)
+# sleep(24 * 60 * 60 - 1)
 
-# while True:
-#     sleep(10)
+while True:
+    sleep(10)
     # for currency in currencies:
     #     bot.sendMessage(chat_id=chat_id, text=price(currency))
     # sleep(600)
